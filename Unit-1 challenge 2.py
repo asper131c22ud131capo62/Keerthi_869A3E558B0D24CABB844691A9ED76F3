a3cb633fa163624d14c8e83f7164b5cf7d568ea7
asper131c22ud131capo62 <145095191+asper131c22ud131capo62@users.noreply.github.com>
2023-09-22 13:14:49 +0530

    Update Unit-1 challenge 2.py

--- Unit-1 challenge 2.py
+++ Unit-1 challenge 2.py
@@ -1,32 +1,12 @@
-class BankAccount:
-    def __init__(self, account_number, account_holder_name, initial_balance):
-        self._account_number = account_number
-        self._account_holder_name = acount_holder_name
-        self._account_balance = initial_balance
+def is_leap_year(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
 
-    def deposit(self, amount):
-        if amount > 0:
-            self._account_balance += amount
-            print(f"Deposited ${amount}. New balance: ${self._account_balance}")
-        else:
-            print("Invalid deposit amount. Please enter a positive amount.")
-
-    def withdraw(self, amount):
-        if 0 < amount <= self._account_balance:
-            self._account_balance -= amount
-            print(f"Withdrew ${amount}. New balance: ${self._account_balance}")
-        else:
-            print("Insufficient funds or invalid withdrawal amount.")
-
-    def display_balance(self):
-        print(f"Account Balance for {self._account_holder_name}: ${self._account_balance}")
-
-
-# Testing the BankAccount class
-if __name__ == "__main__":
-    account = BankAccount("1234567890", "John Doe", 1000.00)
-    
-    account.display_balance()  # Display initial balance
-    account.deposit(500.00)    # Deposit $500
-    account.withdraw(200.00)   # Withdraw $200
-    account.display_balance()  # Display updated balanc
+# Replace '2024' with the year you want to check
+year_to_check = 2024
+if is_leap_year(year_to_check):
+    print(year_to_check, "is a leap year.")
+else:
+    print(year_to_check, "is not 
